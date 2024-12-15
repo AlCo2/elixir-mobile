118c318b518678b122fc2e02e06857dbb341f7f3
Alco2 <abdou.lahboub.wwe@gmail.com>
2024-12-15 19:24:47 +0100

    add products images slider - close #9

--- pages/Product.js
+++ pages/Product.js
@@ -1,17 +1,59 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Image, ScrollView, View } from 'react-native';
-import { Button, Chip, Text } from 'react-native-paper';
+import { Button, Chip, IconButton, Text } from 'react-native-paper';
 import { API_URL } from '@env';
 import { CartContext } from '../context/cartContext';
+import Swiper from 'react-native-swiper';
+import * as SecureStore from 'expo-secure-store';
+import { addProductToFavourit } from '../api/favourit';
+import { useNavigation } from '@react-navigation/native';
+import { showMessage } from 'react-native-flash-message';
 
 const Product = ({route}) => {
-  const { addToCart } = useContext(CartContext);
+  const navigation = useNavigation();
+  const { addToCart, favourites, setFavourites } = useContext(CartContext);
   const { product } = route.params;
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  const addToFav = async () =>{
+    console.log('here');
+    const token = SecureStore.getItem('token');
+    if (token)
+    {
+        if (isFavourite)
+        {
+            const updatedFavourites = favourites.filter(p => p.id !== product.id);
+            setFavourites(updatedFavourites);
+        }
+        else
+        {
+            setFavourites(favourites=>[...favourites, product]);
+        }
+        setIsFavourite(!isFavourite);
+    }
+    await addProductToFavourit(token, product.id).catch((error)=>{
+        if (error.response.status===401)
+        {
+            navigation.navigate('Login');
+        }
+    });
+  }
+  useEffect(()=>{
+    setIsFavourite(favourites.some(p => p.id === product.id))
+  }, [favourites])
   return (
     <ScrollView>
-      <View style={{backgroundColor:'white'}}>
-        <Image style={{height:400, width:'100%'}} source={{uri:`${API_URL}${product.images[0].url}`}}/>
+      <View style={{position:'absolute', top:300, right:20, zIndex:1}}>
+        <IconButton icon="heart" iconColor={isFavourite?"red":"gray"} style={{ borderRadius:10}} size={30} onPress={()=>addToFav()}/>
       </View>
+      <Swiper style={{height:400}}>
+        <View style={{backgroundColor:'white'}}>
+          <Image style={{height:400, width:'100%'}} source={{uri:`${API_URL}${product.images[0].url}`}}/>
+        </View>
+        <View style={{backgroundColor:'white'}}>
+          <Image style={{height:400, width:'100%'}} source={{uri:`${API_URL}${product.images[1].url}`}}/>
+        </View>
+      </Swiper>
       <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center', margin:20}}>
         <Text style={{fontWeight:'bold', width:'75%', fontSize:18}}>{product.title}</Text>
         {product.promotion?
@@ -35,7 +77,7 @@ const Product = ({route}) => {
         </View>
       </View>
       <View  style={{margin:20, alignItems:'center'}}>
-          <Button onPress={()=>addToCart(product)} style={{backgroundColor:'black', borderRadius:20, width:'100%'}} labelStyle={{fontSize:20, paddingVertical:10, fontWeight:'bold'}} mode='contained'>Add to Cart</Button>
+          <Button onPress={()=>{addToCart(product);showMessage({message:'success',description:`${product.title} added to cart successfully`, type:'success'})}} style={{backgroundColor:'black', borderRadius:20, width:'100%'}} labelStyle={{fontSize:20, paddingVertical:10, fontWeight:'bold'}} mode='contained'>Add to Cart</Button>
       </View>
     </ScrollView>
   )
